60b77957c4243b9689b8bef6889401f25704d41f
wangyingtao <wangyingtao@deepin.com>
2016-10-27 13:03:11 +0800

    modified for lava run
    Change-Id: I95c59fdaea660c655b9eb15bd3ba2acc5fd0b87c

---
+++ lava_run.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import unittest
+import json
+
+idfile = "id.txt"
+resultfile = "result.txt"
+
+from ddedock import testFashionExistLeft
+
+def getIdFile():
+    try:
+        f = open(idfile, "r")
+        content = f.read()
+        idcontent = json.loads(content, "UTF-8")
+    except:
+        print("Open file %s failed." % idfile)
+        return None
+
+    idlist = idcontent["test_id"]
+    return idlist
+
+def main():
+    idlist = getIdFile()
+    print(idlist)
+
+    if idlist == None or type(idlist) != list or len(idlist) == 0:
+        print("Can't get the idlist in file %s." % idfile)
+        exit(1)
+
+    allclasses = []
+
+    # add ddedock classes
+    if testFashionExistLeft.caseid in idlist:
+        allclasses.append(testFashionExistLeft.FashionExistLeft)
+
+    if len(allclasses) == 0:
+        print("All classes list is zero.")
+        print("Exit.")
+        exit(1)
+
+    for c in allclasses:
+        suite = c.suite()
+        unittest.TextTestRunner(resultclass=c.MyTestResult).run(suite)
+
+if __name__ == "__main__":
+    main()
