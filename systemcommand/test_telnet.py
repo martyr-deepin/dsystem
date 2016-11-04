bc00f366f5339478e3680d8be61c84c3e4172e43
mayuhope <maggiieyu@gmail.com>
2016-11-04 14:11:44 +0800

    Add telnet case
    Change-Id: I9d1cc6bd38b4ac693f1a1800eda2608697959bea

---
+++ systemcommand/test_telnet.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# _*_ coding: utf-8 _*_
+
+import unittest
+from subprocess import getstatusoutput,check_call
+from time import sleep
+from lib import runner
+from lib import utils
+
+result = True
+caseid = '69020'
+casename = 'all-2586:使用telnet命令连接到远程主机'
+
+class Telnet(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        global result
+        utils.commitresult(caseid,result)
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_check_telnet(self):
+        (status,output) = getstatusoutput('dpkg -l telnet')
+        self.assertEqual(0,status)
+        dpkg_result = output.split('\n')
+        dpkg_status = (dpkg_result[5].split(' '))[0]
+        if dpkg_status == 'un':
+            return_code = check_call('sudo apt-get install telnet -y',shell=True)
+            self.assertEqual(0,return_code)
+        else:
+            pass
+
+    def test_run_telnet(self):
+        (status,output) = getstatusoutput('telnet')
+        self.assertEqual(127,status)
+
+    def suite():
+        suite = unittest.TestSuite()
+        suite.addTest(Telnet('test_check_telnet'))
+        suite.addTest(Telnet('test_run_telnet'))
+        return suite
+
+    class MyTestResult(runner.MyTextTestResult):
+        def addError(self, test, err):
+            super(Telnet.MyTestResult, self).addError(test, err)
+            global result
+            result = result and False
+
+        def addFailure(self, test, err):
+            super(Telnet.MyTestResult, self).addError(test, err)
+            global result
+            result = result and False
+
+if __name__ == "__main__":
+    unittest.TextTestRunner(resultclass=Telnet.MyTestResult).run(Telnet.suite())
---
+++ systemcommand/test_traceroute.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# _*_ coding: utf-8 _*_
+
+import unittest
+from subprocess import getstatusoutput,check_call
+from time import sleep
+from lib import runner
+from lib import utils
+
+result = True
+caseid = '69024'
+casename = 'all-2587:使用命令traceroute网络节点间的通讯'
+
+class Traceroute(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        global result
+        utils.commitresult(caseid,result)
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_check_traceroute(self):
+        (status,output) = getstatusoutput('dpkg -l traceroute')
+        self.assertEqual(0,status)
+        dpkg_result = output.split('\n')
+        dpkg_status = (dpkg_result[5].split(' '))[0]
+        if dpkg_status != 'ii':
+            return_code = check_call('sudo apt-get install inetutils-traceroute traceroute  -y',shell=True)
+            self.assertEqual(0,return_code)
+        else:
+            pass
+
+    def test_run_traceroute(self):
+        (status,output) = getstatusoutput('traceroute www.baidu.com')
+        self.assertEqual(0,status)
+
+    def suite():
+        suite = unittest.TestSuite()
+        suite.addTest(Traceroute('test_check_traceroute'))
+        suite.addTest(Traceroute('test_run_traceroute'))
+        return suite
+
+    class MyTestResult(runner.MyTextTestResult):
+        def addError(self, test, err):
+            super(Traceroute.MyTestResult, self).addError(test, err)
+            global result
+            result = result and False
+
+        def addFailure(self, test, err):
+            super(Traceroute.MyTestResult, self).addError(test, err)
+            global result
+            result = result and False
+
+if __name__ == "__main__":
+    unittest.TextTestRunner(resultclass=Traceroute.MyTestResult).run(Traceroute.suite())
