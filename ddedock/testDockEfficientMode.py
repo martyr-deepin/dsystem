4b18a4f62ca0952b674cdcbb70eedb182cb2c9b1
qiujieqiong <qiujieqiong@linuxdeepin.com>
2016-12-23 15:40:16 +0800

    Fix mutiple caseid in testDockEfficientMode.py
    Change-Id: Ie7dda109b2f29b368acc61e6e6c73e9c233fdbaf

--- ddedock/testDockEfficientMode.py
+++ ddedock/testDockEfficientMode.py
@@ -9,15 +9,13 @@ from lib import runner
 from dogtail import rawinput
 from dogtail.tree import *
 
-casename = "all-2485:高效模式上方显示测试"
 
-class DockEfficientMode(unittest.TestCase):
+
+class DockEfficientModeTop(unittest.TestCase):
     caseid = '68464'
+    casename = "all-2485:高效模式上方显示测试"
     @classmethod
     def setUpClass(cls):
-        cls.casename_2 = "all-2486:高效模式下方显示测试"
-        cls.casename_3 = "all-2487:高效模式左方显示测试"
-        cls.casename_4 = "all-2484:高效模式右方显示测试"
         cls.ddedockobject = utils.getDdeDockObject()
         cls.defaultdisplaymode = utils.getDdeDockDisplayMode()
         cls.defaultposition = utils.getDdeDockPosition()
@@ -91,6 +89,53 @@ class DockEfficientMode(unittest.TestCase):
         self.assertTrue(height > 1)
         self.assertTrue(0 == yp)
 
+    def suite():
+        suite = unittest.TestSuite()
+        suite.addTest(DockEfficientModeTop('testChangePositionTop'))
+        return suite
+
+class DockEfficientModeRight(unittest.TestCase):
+    caseid = '68473'
+    casename = "all-2484:高效模式右方显示测试"
+    @classmethod
+    def setUpClass(cls):
+        cls.ddedockobject = utils.getDdeDockObject()
+        cls.defaultdisplaymode = utils.getDdeDockDisplayMode()
+        cls.defaultposition = utils.getDdeDockPosition()
+        cls.defaulthidemode = utils.getDdeDockHideMode()
+        cls.dockmenuname = "DesktopMenu"
+
+        if utils.dock.displaymode_efficient != cls.defaultdisplaymode:
+            utils.setDdeDockDisplayMode(utils.dock.displaymode_efficient)
+
+        if utils.dock.position_bottom != cls.defaultposition:
+            utils.setDdeDockPosition(utils.dock.position_bottom)
+
+        if utils.dock.hidemode_keepshowing != cls.defaulthidemode:
+            utils.setDdeDockHideMode(utils.dock.hidemode_keepshowing)
+
+
+    @classmethod
+    def tearDownClass(cls):
+
+        if utils.getDdeDockDisplayMode() != utils.dock.displaymode_fashion:
+            utils.setDdeDockDisplayMode(utils.dock.displaymode_fashion)
+
+        if utils.getDdeDockPosition() != utils.dock.position_bottom:
+            utils.setDdeDockPosition(utils.dock.position_bottom)
+
+        if utils.getDdeDockHideMode() != utils.dock.hidemode_keepshowing:
+            utils.setDdeDockHideMode(utils.dock.hidemode_keepshowing)
+
+    def testDockMenuExist(self):
+        try:
+            dockmenuapp = root.application('deepin-menu', '/usr/lib/deepin-menu')
+            dockmenu = dockmenuapp.child(self.dockmenuname)
+        except:
+            self.assertTrue(False, "Can not find the dock menu")
+
+    def testRightClickOnDock(self, x, y):
+        rawinput.click(x, y, 3)
     def testChangePositionRight(self):
         try:
             dock = self.ddedockobject.child(utils.dock.mainwindowname)
@@ -125,6 +170,53 @@ class DockEfficientMode(unittest.TestCase):
         self.assertTrue(height > 1)
         self.assertTrue(width == utils.resolution.width - xp)
 
+    def suite():
+        suite = unittest.TestSuite()
+        suite.addTest(DockEfficientModeRight('testChangePositionRight'))
+        return suite
+
+class DockEfficientModeLeft(unittest.TestCase):
+    caseid = '68470'
+    casename = "all-2487:高效模式左方显示测试"
+    @classmethod
+    def setUpClass(cls):
+        cls.ddedockobject = utils.getDdeDockObject()
+        cls.defaultdisplaymode = utils.getDdeDockDisplayMode()
+        cls.defaultposition = utils.getDdeDockPosition()
+        cls.defaulthidemode = utils.getDdeDockHideMode()
+        cls.dockmenuname = "DesktopMenu"
+
+        if utils.dock.displaymode_efficient != cls.defaultdisplaymode:
+            utils.setDdeDockDisplayMode(utils.dock.displaymode_efficient)
+
+        if utils.dock.position_bottom != cls.defaultposition:
+            utils.setDdeDockPosition(utils.dock.position_bottom)
+
+        if utils.dock.hidemode_keepshowing != cls.defaulthidemode:
+            utils.setDdeDockHideMode(utils.dock.hidemode_keepshowing)
+
+
+    @classmethod
+    def tearDownClass(cls):
+
+        if utils.getDdeDockDisplayMode() != utils.dock.displaymode_fashion:
+            utils.setDdeDockDisplayMode(utils.dock.displaymode_fashion)
+
+        if utils.getDdeDockPosition() != utils.dock.position_bottom:
+            utils.setDdeDockPosition(utils.dock.position_bottom)
+
+        if utils.getDdeDockHideMode() != utils.dock.hidemode_keepshowing:
+            utils.setDdeDockHideMode(utils.dock.hidemode_keepshowing)
+
+    def testDockMenuExist(self):
+        try:
+            dockmenuapp = root.application('deepin-menu', '/usr/lib/deepin-menu')
+            dockmenu = dockmenuapp.child(self.dockmenuname)
+        except:
+            self.assertTrue(False, "Can not find the dock menu")
+
+    def testRightClickOnDock(self, x, y):
+        rawinput.click(x, y, 3)
     def testChangePositionLeft(self):
         try:
             dock = self.ddedockobject.child(utils.dock.mainwindowname)
@@ -158,6 +250,53 @@ class DockEfficientMode(unittest.TestCase):
         self.assertTrue(height > 1)
         self.assertTrue(0 == xp)
 
+    def suite():
+        suite = unittest.TestSuite()
+        suite.addTest(DockEfficientModeLeft('testChangePositionLeft'))
+        return suite
+
+class DockEfficientModeBottom(unittest.TestCase):
+    caseid = '68467'
+    casename = "all-2486:高效模式下方显示测试"
+    @classmethod
+    def setUpClass(cls):
+        cls.ddedockobject = utils.getDdeDockObject()
+        cls.defaultdisplaymode = utils.getDdeDockDisplayMode()
+        cls.defaultposition = utils.getDdeDockPosition()
+        cls.defaulthidemode = utils.getDdeDockHideMode()
+        cls.dockmenuname = "DesktopMenu"
+
+        if utils.dock.displaymode_efficient != cls.defaultdisplaymode:
+            utils.setDdeDockDisplayMode(utils.dock.displaymode_efficient)
+
+        if utils.dock.position_bottom != cls.defaultposition:
+            utils.setDdeDockPosition(utils.dock.position_bottom)
+
+        if utils.dock.hidemode_keepshowing != cls.defaulthidemode:
+            utils.setDdeDockHideMode(utils.dock.hidemode_keepshowing)
+
+
+    @classmethod
+    def tearDownClass(cls):
+
+        if utils.getDdeDockDisplayMode() != utils.dock.displaymode_fashion:
+            utils.setDdeDockDisplayMode(utils.dock.displaymode_fashion)
+
+        if utils.getDdeDockPosition() != utils.dock.position_bottom:
+            utils.setDdeDockPosition(utils.dock.position_bottom)
+
+        if utils.getDdeDockHideMode() != utils.dock.hidemode_keepshowing:
+            utils.setDdeDockHideMode(utils.dock.hidemode_keepshowing)
+
+    def testDockMenuExist(self):
+        try:
+            dockmenuapp = root.application('deepin-menu', '/usr/lib/deepin-menu')
+            dockmenu = dockmenuapp.child(self.dockmenuname)
+        except:
+            self.assertTrue(False, "Can not find the dock menu")
+
+    def testRightClickOnDock(self, x, y):
+        rawinput.click(x, y, 3)
     def testChangePositionBottom(self):
         try:
             dock = self.ddedockobject.child(utils.dock.mainwindowname)
@@ -202,14 +341,11 @@ class DockEfficientMode(unittest.TestCase):
 
     def suite():
         suite = unittest.TestSuite()
-
-        # Efficient
-        suite.addTest(DockEfficientMode('testChangePositionTop'))
-        suite.addTest(DockEfficientMode('testChangePositionRight'))
-        suite.addTest(DockEfficientMode('testChangePositionLeft'))
-        suite.addTest(DockEfficientMode('testChangePositionBottom'))
-
+        suite.addTest(DockEfficientModeBottom('testChangePositionBottom'))
         return suite
 
 if __name__ == "__main__":
-    executeTestCase.runTest(DockEfficientMode)
+    executeTestCase.runTest(DockEfficientModeTop)
+    executeTestCase.runTest(DockEfficientModeRight)
+    executeTestCase.runTest(DockEfficientModeLeft)
+    executeTestCase.runTest(DockEfficientModeBottom)
