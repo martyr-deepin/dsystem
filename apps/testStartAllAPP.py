4fd8aef465da8d3c6c6640f61097c9da9ed7a4e8
mayuhope <maggiieyu@gmail.com>
2016-12-30 14:17:01 +0800

    Updata case and json
    Change-Id: Ie4bfe019f40ab7ad9ac92a13c0619228d708abb6

--- apps/testStartAllAPP.py
+++ apps/testStartAllAPP.py
@@ -31,18 +31,16 @@ class StartAllAPP(unittest.TestCase):
         pass
 
 
-    def terminate_deepinscreenshot(self,pname):
+    def terminate_deepinscreenshot(self):
         p = PyKeyboard()
         p.press_key('Return')
         p.release_key('Return')
         time_now = time.strftime('%Y%m%d%H%M%S', time.localtime())
-        print(time_now)
-        print(type(time_now))
         picture_save_path = os.path.expandvars('$HOME') + '/Desktop/' + '深度截图' + time_now + '.png'
         print(picture_save_path)
-        print(type(picture_save_path))
         time.sleep(2)
         save_bool = os.path.exists(picture_save_path)
+        print(save_bool)
         self.assertTrue(save_bool)
 
     def testRunAPP(self):
@@ -53,10 +51,10 @@ class StartAllAPP(unittest.TestCase):
                 continue
             launcher.searchApp(k)
             time.sleep(1)
-            launcher.launcherObj.child(k).click()
-            time.sleep(5)
+            launcher.launcherObj.child(k).click()   
+            time.sleep(10)
             if k ==  '深度截图':
-                self.terminate_deepinscreenshot(k)
+                self.terminate_deepinscreenshot()
             else:
                 window_obj = window.findWindow(v)
                 print(window_obj)
