7e441466d923faf00836f52604dde1b76d522389
wangyingtao <wangyingtao@deepin.com>
2017-04-19 10:25:58 +0800

    update for checking other language
    Change-Id: I17d64f36a300178264811da53c042598053ea6c3

--- dde_control_center/testLang.py
+++ dde_control_center/testLang.py
@@ -10,30 +10,36 @@ from lib import dde_control_center
 from lib import LangSelector
 from subprocess import getstatusoutput as rt
 
-LANGUAGELIST = ['am_ET',
-                'ast_ES.UTF-8',
+LANGUAGELIST = ['zh_CN.UTF-8',
+                'zh_TW.UTF-8',
+                'zh_HK.UTF-8',
+                'id_ID.UTF-8',
+                'ms_MY.UTF-8',
                 'cs_CZ.UTF-8',
                 'da_DK.UTF-8',
                 'de_DE.UTF-8',
+                'el_GR.UTF-8',
                 'es_ES.UTF-8',
                 'fr_FR.UTF-8',
-                'gl_ES.UTF-8',
                 'hr_HR.UTF-8',
-                'hu_HU.UTF-8',
                 'it_IT.UTF-8',
-                'lt_LT.UTF-8',
-                'ms_MY.UTF-8',
-                'nb_NO.UTF-8',
+                'hu_HU.UTF-8',
                 'nl_NL.UTF-8',
                 'pl_PL.UTF-8',
                 'pt_PT.UTF-8',
                 'pt_BR.UTF-8',
                 'ru_RU.UTF-8',
+                'ro_RO.UTF-8',
                 'sk_SK.UTF-8',
-                'sl_SI.UTF-8',
+                'fi_FI.UTF-8',
+                'sv_SE.UTF-8',
                 'tr_TR.UTF-8',
-                'zh_CN.UTF-8',
-                'zh_TW.UTF-8']
+                'uk_UA.UTF-8',
+                'bg_BG.UTF-8',
+                'ja_JP.UTF-8',
+                'ko_KR.UTF-8',
+                'hi_IN',
+                'am_ET']
 
 class TestLanguage(unittest.TestCase):
     caseid = '999999'
@@ -67,20 +73,26 @@ class TestLanguage(unittest.TestCase):
 
         langs = []
         for k, v in langlist:
-            print(k)
+            # print(k)
             langs.append(k)
 
         for lang in LANGUAGELIST:
             self.assertTrue(lang in langs)
 
+        faillist = []
+
         for lang in LANGUAGELIST:
             os.putenv("LANG", lang)
             os.putenv("LANGUAGE", lang.split(".")[0])
             time.sleep(1)
-            (status, output) = rt("./testLanguage")
+            (status, output) = rt("./testLanguage dde-dock")
+            if 0 == status:
+                faillist.append(lang)
+
             print(output)
-            self.assertTrue(1 == status, "Test language %s failed." % lang)
             time.sleep(2)
+
+        self.assertTrue(0 == len(faillist), "Test language %s failed." % str(faillist))
 
     def suite():
         suite = unittest.TestSuite()
