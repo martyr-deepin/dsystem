0161a0d61d56631a2ea34f698772504160e9d7c7
wangyingtao <wangyingtao@deepin.com>
2017-05-04 14:42:07 +0800

    add Daemon Dock lib
    Change-Id: Icc32e97142d08fb94b5fe4870fe77ca72d81358d

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,10 +1,12 @@
 __all__ = ['do_polkit_agent',
+           'DaemonDock',
            'Dock',
            'LangSelector',
            'Launcher',
            'runTest']
 
 from .com_deepin_daemon_LangSelector import LangSelector
+from .com_deepin_dde_daemon_Dock import DaemonDock
 from .dde_dock import Dock
 from .executeTestCase import runTest
 from .launcher import Launcher
---
+++ lib/com_deepin_dde_daemon_Dock.py
@@ -0,0 +1,22 @@
+#!/usr/bin/evn python3
+# -*- coding: utf-8 -*-
+
+import dbus
+
+class DaemonDock:
+    def __init__(self):
+        self.dbus_name = "com.deepin.dde.daemon.Dock"
+        self.obj_path  = "/com/deepin/dde/daemon/Dock"
+        self.interface = "com.deepin.dde.daemon.Dock"
+
+        self.session_bus = dbus.SessionBus()
+        self.session_obj = self.session_bus.get_object(self.dbus_name,
+                self.obj_path)
+        self.ifc_properties = dbus.Interface(self.session_obj,
+                dbus_interface=dbus.PROPERTIES_IFACE)
+        self.ifc_methods    = dbus.Interface(self.session_obj,
+                dbus_interface=self.interface)
+
+    def getDockedApps(self):
+        return self.ifc_properties.Get(self.interface,
+                "DockedApps")
