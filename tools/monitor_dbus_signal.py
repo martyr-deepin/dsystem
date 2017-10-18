c60f11f76ddef58f4475ee2c14f6df5678090312
wangyingtao <wangyingtao@deepin.com>
2017-10-18 15:22:00 +0800

    monitor special signal
    Change-Id: Ib73aa467591f03cd37db398462e752441e0691ed

--- tools/monitor_dbus_signal.py
+++ tools/monitor_dbus_signal.py
@@ -9,13 +9,17 @@ def msg_cb(bus, msg):
     args = msg.get_args_list()
     print(args)
 
+def signal_handler(string_EntryRemoved):
+    print(string_EntryRemoved)
+
 if __name__ == '__main__':
     DBusGMainLoop(set_as_default=True)
     bus = dbus.SessionBus()
 
     string = "type='signal',sender='com.deepin.dde.daemon.Dock',interface='com.deepin.dde.daemon.Dock'"
     bus.add_match_string(string)
-    bus.add_message_filter(msg_cb)
+    #bus.add_message_filter(msg_cb)
+    bus.add_signal_receiver(signal_handler, signal_name="EntryRemoved")
 
     mainloop = GObject.MainLoop()
     mainloop.run()
